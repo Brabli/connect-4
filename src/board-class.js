99b7e7a213e9338bd58063ed2aa9634186fe337a
Brabli <67018167+Brabli@users.noreply.github.com>
2021-01-02 18:33:59 +0000

    Remove excess code from board class

--- src/board-class.js
+++ src/board-class.js
@@ -16,64 +16,56 @@ class Board {
 
   move(column, symbol) {
     // Returns false if specified column is full.
-    if (this.get(0, column) !== '-') {
-      console.log(`Column ${column} is full!`);
-      return false;
-    }
+    if (this.get(0, column) !== '-') return false;
     // Otherwise move and return true.
-    for (let i = 5; i >= 0; i--) {
-      if (this.get(i, column) === '-') {
-        this.set(i, column, symbol);
+    for (let row = 5; row >= 0; row--) {
+      if (this.get(row, column) === '-') {
+        this.set(row, column, symbol);
         return true;
       }
     }
   }
   
-  // Scans board creating an array of lines. If bool is true, console.log each line.
-  scan(bool = false) {
+  // Scans board creating an array of line objects. If log is true, console.log each line.
+  scan(log = false) {
     let rows = [], cols = [], diags = [];
-    let lineString, lineCoords;
+
     // ROWS
     for (let r = 0; r < 6; r++) {
       for (let c = 0; c < 3; c++) {
-        lineString = this.board[r][c] + this.board[r][c + 1] + this.board[r][c + 2] + this.board[r][c + 3] + this.board[r][c + 4];
-        lineCoords = [[r, c], [r, c + 1], [r, c + 2], [r, c + 3], [r, c + 4]];
-        const line = new Line(lineString, lineCoords, this.board);
-        if (bool) console.log(line);
-        rows.push(line);
+        const lineString = this.get(r, c) + this.get(r, c + 1) + this.get(r, c + 2) + this.get(r, c + 3) + this.get(r, c + 4);
+        const lineCoords = [[r, c], [r, c + 1], [r, c + 2], [r, c + 3], [r, c + 4]];
+        rows.push( new Line(lineString, lineCoords, this.board) );
       }
     }
     // COLS
     for (let c = 0; c < 7; c++) {
       for (let r = 0; r < 2; r++) {
-        let lineString = this.board[r][c] + this.board[r + 1][c] + this.board[r + 2][c] + this.board[r + 3][c] + this.board[r + 4][c];
-        lineCoords = [[r, c], [r + 1, c], [r + 2, c], [r + 3, c], [r + 4, c]]
-        const line = new Line(lineString, lineCoords, this.board);
-        if (bool) console.log(line);
-        cols.push(line);
+        const lineString = this.get(r, c) + this.get(r + 1, c) + this.get(r + 2, c) + this.get(r + 3, c) + this.get(r + 4, c);
+        const lineCoords = [[r, c], [r + 1, c], [r + 2, c], [r + 3, c], [r + 4, c]]
+        cols.push( new Line(lineString, lineCoords, this.board) );
       }
     }
     // DIAGS BACK
     for (let r = 0; r < 2; r++) {
       for (let c = 0; c < 3; c++) {
-        lineString = this.board[r][c]+ this.board[r + 1][c + 1] + this.board[r + 2][c + 2] + this.board[r + 3][c + 3] + this.board[r + 4][c + 4];
-        lineCoords = [[r, c], [r + 1, c + 1], [r + 2, c + 2], [r + 3, c + 3], [r + 4, c + 4]];
-        const line = new Line(lineString, lineCoords, this.board);
-        if (bool) console.log(line);
-        diags.push(line);
+        const lineString = this.get(r, c) + this.get(r + 1, c + 1) + this.get(r + 2, c + 2) + this.get(r + 3, c + 3) + this.get(r + 4, c + 4);
+        const lineCoords = [[r, c], [r + 1, c + 1], [r + 2, c + 2], [r + 3, c + 3], [r + 4, c + 4]];
+        diags.push( new Line(lineString, lineCoords, this.board) );
       }
     }
     // DIAGS Front
     for (let r = 0; r < 2; r++) {
       for (let c = 0; c < 3; c++) {
-        lineString = this.board[r][c + 4] + this.board[r + 1][c + 3] + this.board[r + 2][c + 2] + this.board[r + 3][c + 1] + this.board[r + 4][c];
-        lineCoords = [[r, c + 4], [r + 1, c + 3], [r + 2, c + 2], [r + 1, c + 1], [r + 4, c]];
-        const line = new Line(lineString, lineCoords, this.board);
-        if (bool) console.log(line);
-        diags.push(line);
+        const lineString = this.get(r,c + 4) + this.get(r + 1, c + 3) + this.get(r + 2, c + 2) + this.get(r + 3, c + 1) + this.get(r + 4, c);
+        const lineCoords = [[r, c + 4], [r + 1, c + 3], [r + 2, c + 2], [r + 1, c + 1], [r + 4, c]];
+        diags.push( new Line(lineString, lineCoords, this.board) );
       }
     }
-    return [...rows, ...cols, ...diags];
+    
+    const allLines = [...rows, ...cols, ...diags];
+    if (log) allLines.forEach(line => console.log(line));
+    return allLines;
   }
 
   reset() {
