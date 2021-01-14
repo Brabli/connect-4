70362063e32a5dcd1ff5daee809c59e535562b82
Brabli <67018167+Brabli@users.noreply.github.com>
2021-01-14 14:06:36 +0000

    Psuedo code for moving algorithm. Will be deleted at some point.

---
+++ psuedo.js
@@ -0,0 +1,23 @@
+// X TO MOVE.
+
+// Check for a winner.
+this.winningX
+this.winningY
+
+// Check to make sure a move can be played.
+// If movableColumns.length is 1, move there.
+this.movableColumns
+
+// Check to see if a winning move can be made.
+this.winningColX 
+// Check to see if a winning move for Y can be blocked.
+this.winningColY
+
+// Check to see if a ULOT can be made or blocked.
+// Both need to be future checked to make sure 
+this.unblockableRowOfThreeXColumn
+this.unblockableRowOfThreeYColumn
+this.rowOfThreeXColumns
+this.rowOfThreeYColumns 
+this.rowOfTwoXColumns
+this.rowOfTwoYColumns
